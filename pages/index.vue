b7d5959f12ad13ff798bfaf4198b61c2eed3f0f0
mandylai <user@email.com>
2024-11-10 12:41:04 +0800

    add pages index

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup >
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h2>Page首頁</h2>
+    <p>目前路由路徑-route.fullPath: {{ route.fullPath }} </p>
+  </div>
+</template>
+
+<style scoped></style>
